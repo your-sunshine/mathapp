f796758ee302f8fa297e7fe0d3390501a3126b4d
your-sunshine <sunnysunshine876@gmail.com>
2026-08-27 15:30:26 +0700

    Update page switch logic and file reference

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
 
 if st.session_state.logged_in:
-    st.switch_page("pages/app.py")
+    st.switch_page("pages/math-app.py")
 else:
     st.switch_page("pages/login.py")
