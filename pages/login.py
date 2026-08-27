de5512d462f11b50d32863ddeec0c0c55698faa2
your-sunshine <sunnysunshine876@gmail.com>
2026-08-27 15:43:36 +0700

    Update login page to include name input and form
    Refactor login page to use a form for account creation and add name input.

--- pages/login.py
+++ pages/login.py
@@ -13,12 +13,21 @@ st.title("🧮 My Math App")
 st.write("Please login to access the feature!")
 st.divider()
 
-
-email = st.text_input("Email")
-password = st.text_input("Password", type="password")
-confirm_password = st.text_input("Confirm Password", type="password")
-
-if st.button("Create Account", use_container_width=True):
-    st.session_state.logged_in = True
-    st.session_state.current_user = email
-    st.success("Registration successful!")
+with st.form("login"):
+    name = st.text_input("Name")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    confirm_password = st.text_input("Confirm Password", type="password")
+    submitted = st.form_submit_button("Create Account")
+    
+    if submitted:
+        if not (name and email and password and confirm_password) :
+            st.error("Please fill all fields")
+        elif password != confirm_password:
+            st.error("Password and confirm password must be same")
+        else:
+            st.session_state.logged_in = True
+            st.session_state.current_user = name
+            st.success("Registration successful!")
+            time.sleep(5)
+            st.switch_page("pages/math-app.py")
